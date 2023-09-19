9770145309db1a151a4026599a9f1ea8101eb8c7
michellpalma <141769086+michellpalma@users.noreply.github.com>
2023-09-19 14:45:02 -0500

    Add files via upload
    Añadido:
    - Respaldo
    -Asiento

--- c02/silla/silla.js
+++ c02/silla/silla.js
@@ -13,29 +13,51 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //renderer.domElement: lienzo donde el renderizador dibuja su salida
 document.body.appendChild( renderer.domElement );
             
-			
-//BoxGeometry:crea el cubo con sus dimensionjes (Ancho, Altura, Profundidad)
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-            
-//MeshBasicMaterial: Parametro opcional (color:0xffffff(blanca))
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            
+const x=2
+const y=2			
+const z=2
 
-function crear (x, y, z)
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+
+const respaldoG = new THREE.CylinderGeometry(0.4, 0.4, 0.2, 32);
+
+const respaldo = new THREE.Mesh(respaldoG, material);
+scene.add(respaldo);
+respaldo.rotation.set(5,0,0);
+respaldo.position.set(0,0.5,0);
+
+function crearC (a, b, c)
 {
+    //BoxGeometry:crea el cubo con sus dimensionjes (Ancho, Altura, Profundidad)
+    const geometryC = new THREE.BoxGeometry( x, y, z );
+            
+    //MeshBasicMaterial: Parametro opcional (color:0xffffff(blanca))
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
     //Mesh: geometría: (opcional), material: (opcional)
-    nombre = new THREE.Mesh( geometry, material );
-    nombre.scale(x,y,z);
+    const nombre = new THREE.Mesh( geometryC, material );
     
-    //En la escena se añade la funcion del cubo
-	scene.add( nombre );
+   nombre.scale.set(a,b,c)
 
     return nombre;
 }
 
-crear(0.4, 0.4, 0.1);
+asiento = crearC(0.4, 0.1, 0.4);
 
-camera.position.z = 5;
+ //En la escena se añade la funcion del cubo
+ scene.add( asiento );
+    
+ //asiento.scale.set(0.05, 0.5, 0.05);
+/*pata1 = crearC(0.05, 0.05, 0.5);
+pata2 = crearC(0.05, 0.05, 0.5);
+pata3 = crearC(0.05, 0.05, 0.5);
+pata4 = crearC(0.05, 0.05, 0.5);*/
+
+camera.position.z = 6;
+//camera.position.x = 2;
+camera.position.y = 1;
+//pata1.position.set(1,1,1)
 
 renderer.render( scene, camera );
             
