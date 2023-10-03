a5bbf85602002d1e3a5879d9df459fb58b5a46de
michellpalma <141769086+michellpalma@users.noreply.github.com>
2023-10-03 14:20:27 -0500

    Add files via upload

--- c02/tubo/tubo.js
+++ c02/tubo/tubo.js
@@ -8,30 +8,46 @@ document.body.appendChild( renderer.domElement );//renderer.domElement
             
 const curve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(-10, 0, 0),
-    /*new THREE.Vector3(-5, 5, 0),
+    new THREE.Vector3(-5, 5, 0),
     new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(5, -5, 0),*/
+    new THREE.Vector3(5, -5, 0),
     new THREE.Vector3(10, 0, 0)
 ]);
 
 // Crear la geometría del tubo utilizando TubeGeometry
-const tubeGeometry = new THREE.TubeGeometry(curve, 64, 0.5, 8, false);
+const tubeGeometry = new THREE.TubeGeometry(curve, 64, 1, 8, 8, false);
 
 // Crear un material
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 // Crear una malla utilizando la geometría y el material
-const tubeMesh = new THREE.Mesh(tubeGeometry, material);
+const tube = new THREE.Mesh(tubeGeometry, material);
 
 // Añadir la malla a la escena
-scene.add(tubeMesh);
+scene.add(tube);
 
 //Posición de la cámara en el eje z
 camera.position.z = 20;
 
+//Funcion para animar el cubo y hacer que este rote
+function animate() 
+{
+    // requestAnimationFrame: Método que genera animaciones fluidas
+    requestAnimationFrame( animate );
 
-// Renderizar la escena
-renderer.render(scene, camera);
+    //Rotar el tubo en el eje x y y
+    tube.rotation.x += 0.01;
+    tube.rotation.y += 0.01;
+
+    //.render: Renderiza una escena u otro tipo de objeto usando una cámara 
+    //(scene: Object3D, camera: Cámara)
+    renderer.render( scene, camera );
+}
+
+animate();
+
+// Renderizar la escena renderer.render(scene, camera);
+
 
             
             
